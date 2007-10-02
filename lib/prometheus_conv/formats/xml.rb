2af42585421d1fe0650bc3d3b9bf99d53ea71a9f
Jens Wille <jens.wille@uni-koeln.de>
2007-10-02 08:30:26 +0000

    Updated license headers to version 3 of the GPL.

--- lib/prometheus_conv/formats/xml.rb
+++ lib/prometheus_conv/formats/xml.rb
@@ -12,7 +12,7 @@
 #                                                                             #
 # prometheus_conv is free software; you can redistribute it and/or modify it  #
 # under the terms of the GNU General Public License as published by the Free  #
-# Software Foundation; either version 2 of the License, or (at your option)   #
+# Software Foundation; either version 3 of the License, or (at your option)   #
 # any later version.                                                          #
 #                                                                             #
 # prometheus_conv is distributed in the hope that it will be useful, but      #
@@ -21,11 +21,7 @@
 # more details.                                                               #
 #                                                                             #
 # You should have received a copy of the GNU General Public License along     #
-# with prometheus_conv [*]; if not, write to the Free Software Foundation,    #
-# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              #
-#                                                                             #
-# [*] For the complete text of the GNU General Public License see the file    #
-#     COPYING or point your browser at <http://www.gnu.org/licenses/gpl.txt>  #
+# with prometheus_conv. If not, see <http://www.gnu.org/licenses/>.           #
 #                                                                             #
 ###############################################################################
 #++
--- lib/prometheus_conv/util.rb
+++ lib/prometheus_conv/util.rb
@@ -1,3 +1,31 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of prometheus_conv, the prometheus file converter.              #
+#                                                                             #
+# Copyright (C) 2007 University of Cologne,                                   #
+#                    Albertus-Magnus-Platz,                                   #
+#                    50932 Cologne, Germany                                   #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# prometheus_conv is free software; you can redistribute it and/or modify it  #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# prometheus_conv is distributed in the hope that it will be useful, but      #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with prometheus_conv. If not, see <http://www.gnu.org/licenses/>.           #
+#                                                                             #
+###############################################################################
+#++
+
 module PrometheusConv
 
   module Util
